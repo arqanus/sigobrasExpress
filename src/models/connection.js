e8804edbcc8213399dc03bcb2b3fc8477c1dd6fd
kai <luijhy9234@gmail.com>
2019-02-18 16:32:53 -0500

    github

--- src/models/connection.js
+++ src/models/connection.js
@@ -9,15 +9,20 @@ var pool = mysql.createPool({
     connectionLimit : 500,
     connectTimeout:40000,
     
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'b5ec525fc59630',
+    password: 'e81c813f',
+    database: 'heroku_06daa7d2a5a7a30'
+
     // host: '192.168.1.59',
     // user: 'anton',
     // password: 'antontupuedes',    
     // database: 'sigobras'
 
-    host: 'localhost',
-    user: 'anton',
-    password: 'antontupuedes',    
-    database: 'sigobras'
+    // host: 'localhost',
+    // user: 'anton',
+    // password: 'antontupuedes',    
+    // database: 'sigobras'
     
     
 });    
