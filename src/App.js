a94d7a0d4d67e437917910b71f54bb104446b16b
kai <luijhy9234@gmail.com>
2019-02-20 17:00:49 -0500

    procesos iniciales datos generales

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ const PORT = process.env.PORT || 9000
 
 
 //open cors
+var whitelist = ['http://localhost:9009', 'http://localhost:9001']
 var corsOptions = {
-	origin: 'http://localhost:9009',
-	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
-app.use(cors(corsOptions));
 
 //settings
 app.set('port',PORT);
