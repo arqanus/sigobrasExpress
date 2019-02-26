53c7d65fbe4f86caef05197b015856aabbab2a38
kai <luijhy9234@gmail.com>
2019-02-26 16:42:53 -0500

    test

--- src/models/connection.js
+++ src/models/connection.js
@@ -8,20 +8,20 @@ var connection = mysql.createConnection(
 var pool = mysql.createPool({
    
     
-    // host: 'us-cdbr-iron-east-03.cleardb.net',
-    // user: 'b5ec525fc59630',
-    // password: 'e81c813f',
-    // database: 'heroku_06daa7d2a5a7a30'
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'b5ec525fc59630',
+    password: 'e81c813f',
+    database: 'heroku_06daa7d2a5a7a30'
 
     // host: '192.168.0.4',
     // user: 'kaido',
     // password: 'kaido',    
     // database: 'SIGOBRAS_DB'
 
-    host: 'localhost',
-    user: 'root',
-    password: 'root',    
-    database: 'SIGOBRAS_DB'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'root',    
+    // database: 'SIGOBRAS_DB'
     
     
 });    
