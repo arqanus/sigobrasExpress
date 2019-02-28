815572d7c0083f914fbc4f2f09daf62497e27a19
kai <luijhy9234@gmail.com>
2019-02-28 12:00:14 -0500

    avance corte

--- src/routes/rutasProcesosFisicos.js
+++ src/routes/rutasProcesosFisicos.js
@@ -43,6 +43,33 @@ module.exports = function(app){
 		
 
 	})
+	app.post('/avanceActividadCorte',(req,res)=>{
+			
+		User.getIdHistorial(req.body.id_ficha,(err,data)=>{
+			if(err||data.length==0){ res.status(204).json(err);}
+			else{
+				delete req.body.id_ficha
+				req.body.historialEstados_id_historialEstado = data[0].id_historialEstado
+				
+				var id_actividad = req.body.Actividades_id_actividad
+				User.postAvanceActividad(req.body,(err,data)=>{
+					if(err){ res.status(204).json(err);}
+					else{
+						User.getAvanceById(req.body.Actividades_id_actividad,(err,data)=>{
+							if(err){ res.status(204).json(err);}
+							else{
+								res.json(data);	
+							}
+						})
+						
+					}
+				})
+			}
+		})
+		
+		
+
+	})
 	app.post('/getHistorial',(req,res)=>{
 
 		User.getHistorial(req.body.id_ficha,(err,data)=>{
