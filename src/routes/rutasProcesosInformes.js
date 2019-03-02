a74b43df5b3b377ecf9396ce57ea0d3ac748334c
kai <luijhy9234@gmail.com>
2019-03-02 11:48:19 -0500

    menus

--- src/routes/rutasProcesosInformes.js
+++ src/routes/rutasProcesosInformes.js
@@ -6,13 +6,17 @@ const User = require('../models/procesosInformes');
 module.exports = function(app){
     app.post('/informeControlEjecucionObras',(req,res)=>{
 		// console.log(req.body);
-        
-		User.getinformeControlEjecucionObras(req.body.id_ficha,(err,data)=>{							
-			if(err){ res.status(204).json(err);}
-			else{
-				res.json(data);	
-			}
-
-		})
+    if(req.body.id_ficha == null){
+			res.json("null");		
+		}else{
+			User.getinformeControlEjecucionObras(req.body.id_ficha,(err,data)=>{							
+				if(err){ res.status(204).json(err);}
+				else{
+					res.json(data);	
+				}
+	
+			})
+		}
+		
 	})
 }
