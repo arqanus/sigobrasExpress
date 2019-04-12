faab3284909a48ce6ec51b5042361f45420cff74
kai <luijhy9234@gmail.com>
2019-04-12 10:14:51 -0500

    fix de respuesta posfinacierocorte

--- src/api/Inicio/post/routes/r.post.Inicio.js
+++ src/api/Inicio/post/routes/r.post.Inicio.js
@@ -114,7 +114,65 @@ module.exports = function(app){
 		User.postFinancieroCorte(req.body.monto,req.body.id_historialEstado,(err,data)=>{							
 			if(err){ res.status(204).json(err);}
 			else{
-				res.json(data)
+				if(req.body.id_ficha == null){
+					res.json("null");		
+				}else{
+					
+					
+					User2.getUltimoCorte(req.body.id_ficha,(err,corte)=>{
+						if(err){
+							res.json(err);	
+						}else{
+							// res.json(corte)
+							console.log(req.body.id_ficha,corte.fecha_final);
+							var fecha_inicial = fechaLargaCorta(new Date(corte.fecha_inicial))
+							var fecha_final = fechaLargaCorta(new Date(corte.fecha_final))
+							
+							User2.getAvanceGestionAnterior(req.body.id_ficha,corte.fecha_final,(err,avance)=>{
+								if(err){
+									res.json(err);
+								}else{
+									console.log("avance",avance.avance);
+									corte.fisico_monto = avance.avance||0
+									var avance_Acumulado = 0
+									if(corte.codigo == "C"){
+										avance_Acumulado = corte.fisico_monto
+		
+									}
+									
+									User2.getcronogramaInicio(corte,req.body.id_ficha,corte.fecha_final,(err,data)=>{
+										if(err){
+											res.json(err);
+										}else{
+											if(data=="vacio"){
+												data = {}
+												data.programado_monto_total
+												data.programado_porcentaje_total
+												data.fisico_monto_total
+												data.fisico_porcentaje_total
+												data.financiero_monto_total
+												data.financiero_porcentaje_total
+												data.grafico_programado=[]
+												data.grafico_fisico=[]
+												data.grafico_financiero=[]
+												data.grafico_periodos=[]
+												data.data=[]								
+											}
+											data.fecha_inicial = fecha_inicial
+											data.fecha_final = fecha_final
+											data.avance_Acumulado = avance_Acumulado
+											data.fechaActual = fechaActual()
+											res.json(data)
+										}	
+									
+									})
+								}	
+							
+							})
+						}	
+					
+					})
+				}
 			}
 		})
 	})
