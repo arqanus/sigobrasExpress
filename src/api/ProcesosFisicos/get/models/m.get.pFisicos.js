261b76ea94e9f0ab725114a6ce94a441ceb5e5ff
kai <luijhy9234@gmail.com>
2019-04-18 18:40:28 -0500

    fix de getmateriales porobra

--- src/api/ProcesosFisicos/get/models/m.get.pFisicos.js
+++ src/api/ProcesosFisicos/get/models/m.get.pFisicos.js
@@ -2104,10 +2104,7 @@ userModel.getMaterialesPorObra = (id_ficha,callback)=>{
                     conn.destroy()
                 }else{ 
                     for (let i = 0; i < res.length; i++) {
-                        const fila = res[i];
-                        fila.metrado = formato(fila.metrado)
-                        fila.costo_unitario = formato(fila.costo_unitario)
-                        fila.costo_parcial = formato(fila.costo_parcial)
+                        const fila = res[i];                 
                         fila.cuadrilla_total = formato(fila.cuadrilla_total)
                         fila.cantidad_total = formato(fila.cantidad_total)
                         fila.precio_total = formato(fila.precio_total)
