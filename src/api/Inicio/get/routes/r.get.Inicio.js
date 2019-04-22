a5f741f4651434b444a7a7854902d9f4dccbdbc9
kai <luijhy9234@gmail.com>
2019-04-22 13:57:57 -0500

    fix temp v2 getcronogramaInicio

--- src/api/Inicio/get/routes/r.get.Inicio.js
+++ src/api/Inicio/get/routes/r.get.Inicio.js
@@ -141,6 +141,7 @@ module.exports = function(app){
 							corte.programado_porcentaje = avance.porcentaje||0
 							corte.fisico_monto = avance.valor_total||0
 							corte.fisico_porcentaje = avance.porcentaje||0
+							corte.financiero_porcentaje = (corte.financiero_monto/avance.costo_directo*100)
 							var avance_Acumulado = 0
 							if(corte.codigo == "C"){
 								avance_Acumulado = corte.fisico_monto
