41a89705720730bd03cb142de40e0585c8b28c48
kailucho <luijhy234@gmail.com>
2019-04-26 15:55:31 -0500

    fix de post

--- src/api/GestionTareas/get/routes/r.get.GT.js
+++ src/api/GestionTareas/get/routes/r.get.GT.js
@@ -13,12 +13,12 @@ function daysdifference(date1, date2) {
 }
 
 module.exports = function(app){
-  	app.get('/getTareaProyectos',async (req,res)=>{
+  	app.post('/getTareaProyectos',async (req,res)=>{
 		var proyectos = await User.getTareaProyectos()
 		res.json(proyectos)
 	})
-	app.get('/getTareaCargos',async (req,res)=>{
-		console.log("id_acceso",req);
+	app.post('/getTareaCargos',async (req,res)=>{
+		console.log("dataaaaaaaaaaaaaaaaaaaaaaaaaaa",req.body);
 		try {
 			var nivel = await User.getTareaAccesoCargo(req.body.id_acceso)
 			console.log("nivel",nivel);	
@@ -26,33 +26,27 @@ module.exports = function(app){
 			var cargos = await User.getTareaCargos(req.body.id_acceso,nivel)
 			res.json(cargos)
 		} catch (error) {
-			if (req.boyd.id_acceso == null) {
-				res.status(200).json(error)				
-			} else {
-				
-			}
+			
 			res.status(204).json(error)
 		}
-		
-
 	})
-	app.get('/getTareaUsuariosPorCargo',async (req,res)=>{		
+	app.post('/getTareaUsuariosPorCargo',async (req,res)=>{		
 		var cargos = await User.getTareaUsuariosPorCargo(req.body.id_acceso,req.body.id_Cargo)
 		res.json(cargos)
 	})
-	app.get('/getTareaReceptorPendientes',async (req,res)=>{		
+	app.post('/getTareaReceptorPendientes',async (req,res)=>{		
 		var tareas = await User.getTareaReceptor(req.body.id_acceso,0,0)
 		res.json(tareas)
 	})
-	app.get('/getTareaReceptorProgreso',async (req,res)=>{		
+	app.post('/getTareaReceptorProgreso',async (req,res)=>{		
 		var tareas = await User.getTareaReceptor(req.body.id_acceso,1,99)
 		res.json(tareas)
 	})
-	app.get('/getTareaReceptorTerminadas',async (req,res)=>{		
+	app.post('/getTareaReceptorTerminadas',async (req,res)=>{		
 		var tareas = await User.getTareaReceptor(req.body.id_acceso,100,100)
 		res.json(tareas)
 	})
-	app.get('/getTareaIdTarea',async (req,res)=>{				
+	app.post('/getTareaIdTarea',async (req,res)=>{				
 		var tareas = await User.getTareaIdTarea(req.body.id_tarea,100,100)
 		var diasTotal  = daysdifference(tareas.fecha_inicial,tareas.fecha_final)
 		var diasTranscurridos  = daysdifference(tareas.fecha_inicial,new Date())
