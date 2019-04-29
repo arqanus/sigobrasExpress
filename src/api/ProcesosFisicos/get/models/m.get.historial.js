06b35ab45c4d68eaa1d8de115a46dd9f12c0fbc3
kailucho <luijhy234@gmail.com>
2019-04-29 16:10:09 -0500

    getTareasEmisorCantidades

--- src/api/ProcesosFisicos/get/models/m.get.historial.js
+++ src/api/ProcesosFisicos/get/models/m.get.historial.js
@@ -72,12 +72,12 @@ userModel.getHistorialMeses  = (id_ficha,anyo,callback)=>{
 }
 userModel.getHistorialResumen = (id_ficha,fecha,callback)=>{    
     pool.query("SELECT concat('C-',componentes.numero) nombre, day(avanceactividades.fecha) dia, SUM(avanceactividades.valor*partidas.costo_unitario) valor FROM fichas LEFT JOIN componentes ON componentes.fichas_id_ficha = fichas.id_ficha LEFT JOIN partidas ON partidas.componentes_id_componente = componentes.id_componente LEFT JOIN actividades ON actividades.Partidas_id_partida = partidas.id_partida INNER JOIN avanceactividades ON avanceactividades.Actividades_id_actividad = actividades.id_actividad LEFT JOIN historialestados ON historialestados.Fichas_id_ficha = fichas.id_ficha AND historialestados.fecha_inicial <= avanceactividades.fecha AND avanceactividades.fecha < historialestados.fecha_final LEFT JOIN estados ON estados.id_Estado = historialestados.Estados_id_Estado LEFT JOIN historialactividades ON historialactividades.actividades_id_actividad = actividades.id_actividad WHERE historialactividades.estado IS NULL AND COALESCE(avanceactividades.valor, 0) != 0 AND fichas.id_ficha = ? AND DATE_FORMAT(avanceactividades.fecha, '%Y-%m-01') = DATE_FORMAT(?, '%Y-%m-01') GROUP BY componentes.id_componente , DATE_FORMAT(avanceactividades.fecha, '%Y-%m-%d') order by day(avanceactividades.fecha)",[id_ficha,fecha],(err,res)=>{ 
-        if(err){ console.log(err);
+        if(err){ 
             callback(err.code);
         }else if(res.length==0){
-            console.log("vacio");
             callback(null,"vacio");
         }else{ 
+            callback(null,res)
             //calculando ultimo dia  
             var fecha_array = fecha.split("-")
             var month = fecha_array[1]-1;
@@ -125,16 +125,11 @@ userModel.getHistorialResumen = (id_ficha,fecha,callback)=>{
                 }
             }
             
-            callback(null,{
-                categories,
-                series
-
-            });                    
-            
-            
+            // callback(null,{
+            //     categories,
+            //     series
+            // });                    
         }
-        
-        
     })
 }
 userModel.getHistorialComponentes = (id_ficha,fecha,callback)=>{
