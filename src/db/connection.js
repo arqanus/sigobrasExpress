3ae5aaac390367d9f2ef547711e3a9f33c430cae
kailucho <luijhy234@gmail.com>
2019-04-30 19:00:14 -0500

    db connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -15,17 +15,17 @@ var pool = mysql.createPool({
     // connectTimeout: 1000
 
     /**********config laptop run* */
-    host: '192.168.0.4',
-    user: 'kaido',
-    password: 'kaido',    
-    database: 'SIGOBRAS_DB',
-    connectTimeout: 1000
-
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'password',    
+    // host: '192.168.0.4',
+    // user: 'kaido',
+    // password: 'kaido',    
     // database: 'SIGOBRAS_DB',
-    // connectTimeout: 10000
+    // connectTimeout: 1000
+
+    host: 'localhost',
+    user: 'root',
+    password: 'password',    
+    database: 'SIGOBRAS_DB',
+    connectTimeout: 10000
 });    
 
 pool.getConnection((err, connection) => {
