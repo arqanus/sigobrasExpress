be71d337ea6f9af75a2c602774103c7e6387cb6c
kailucho <luijhy234@gmail.com>
2019-05-02 10:51:44 -0500

    fix getValGeneralPeriodos ultimo dia del anyo

--- src/api/ProcesosFisicos/get/models/m.get.valGeneral.js
+++ src/api/ProcesosFisicos/get/models/m.get.valGeneral.js
@@ -38,7 +38,7 @@ userModel.getValGeneralPeriodos  = (id_ficha,anyo)=>{
                     if(i< res.length-1){
                         fila.fecha_final = res[i+1].fecha_inicial.toLocaleString()
                     }else{
-                        fila.fecha_final = new Date().toLocaleString();
+                        fila.fecha_final = new Date(anyo, 11, 31)
                     }
                 }   
                 return resolve(res)
