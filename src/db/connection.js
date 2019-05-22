bba00f7521db73d3ee1547ffa4846286a9742fc0
kailucho <luijhy234@gmail.com>
2019-05-22 18:47:27 -0500

    db onnection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,44 +1,44 @@
 const mysql = require('mysql');
-
-
-
 var pool = mysql.createPool({
-   
-    //**** config en red trabajo */
-    // host: '192.168.0.4',
-    // user: 'mish',
-    // password: 'mish',    
-    // database: 'SIGOBRAS_DB',
-    // connectTimeout: 1000
-
-    /**********config laptop run* */
-    // host: '192.168.0.4',
-    // user: 'kaido',
-    // password: 'kaido',    
-    // database: 'SIGOBRAS_DB',
-    // connectTimeout: 1000
-
+    //master
     host: 'localhost',
     user: 'root',
     password: 'password',    
-    database: 'SIGOBRAS_DB',
-    connectTimeout: 10000
-});    
-
+    database: 'sigobras_db',
+    // //dev
+    // host: '192.168.0.4',
+    // user: 'kaido',
+    // password: 'kaido',    
+    // database: 'sigobras_db',
+    // //master_test
+    // host: 'localhost',
+    // user: 'kaido',
+    // password: 'kaido',    
+    // database: 'sigobras_db_test',
+    // //dev_test
+    // host: '192.168.0.4',
+    // user: 'kaido',
+    // password: 'kaido',    
+    // database: 'sigobras_db_test',
+    // //mish
+    // host: '192.168.0.4',
+    // user: 'mish',
+    // password: 'mish',    
+    // database: 'sigobras_db_test',
+});
 pool.getConnection((err, connection) => {
-  if (err) {
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-          console.error('Database connection was closed.')
-      }
-      if (err.code === 'ER_CON_COUNT_ERROR') {
-          console.error('Database has too many connections.')
-      }
-      if (err.code === 'ECONNREFUSED') {
-          console.error('Database connection was refused.')
-      }
-  }
-  if (connection) connection.release()
-  return
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) connection.release()
+    return
 })
-
 module.exports = pool;
