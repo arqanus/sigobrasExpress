118b4e5ee4d980025a39191ea5eb87ddf2154c37
kailucho <luijhy234@gmail.com>
2019-05-28 10:03:14 -0500

    fix data

--- src/api/Inicio/post/models/m.post.Inicio.js
+++ src/api/Inicio/post/models/m.post.Inicio.js
@@ -1,53 +1,26 @@
 const pool = require('../../../../db/connection');
 let userModel = {};
-userModel.postcronogramamensual = (data,callback)=>{
-    
-    pool.getConnection(function(err ,conn){
-        if(err){                        
-            callback(err);
-        }
-        else{     
-            //insertar datos query
-            conn.query("INSERT INTO cronogramamensual (fichas_id_ficha,mes,programado, financieroEjecutado) VALUES ? ON DUPLICATE key UPDATE financieroEjecutado = VALUES(financieroEjecutado) ,programado = VALUES(programado) ",[data],(error,res)=>{ 
-                if(error){
-                    console.log(error);                    
-                    callback(error.code);
-                }else{
-                   
-                    console.log("res",res); 
-                    callback(null,res);
-                    conn.destroy()
-                }
-                
-                
-            })
-        }                
+userModel.postcronogramamensual = (data, callback) => {
+    return new Promise((resolve, reject) => {
+        pool.query("INSERT INTO cronogramamensual (fichas_id_ficha,mes,programado, financieroEjecutado) VALUES ? ON DUPLICATE key UPDATE financieroEjecutado = VALUES(financieroEjecutado) ,programado = VALUES(programado) ", [data], (error, res) => {
+            if (error) {
+                reject(error.code);
+            } else {
+                resolve(res);
+            }
+        })
     })
 }
 
-userModel.postFinancieroCorte = (monto,id_historialEstado,callback)=>{
-    
-    pool.getConnection(function(err ,conn){
-        if(err){                        
-            callback(err);
-        }
-        else{     
-            //insertar datos query
-            conn.query("update historialEstados set monto =? where id_historialEstado=?",[monto,id_historialEstado],(error,res)=>{ 
-                if(error){
-                    console.log(error);                    
-                    callback(error.code);
-                    conn.destroy()
-                }else{
-                   
-                    console.log("res",res); 
-                    callback(null,res);
-                    conn.destroy()
-                }
-                
-                
-            })
-        }                
+userModel.postFinancieroCorte = (monto, id_historialEstado) => {
+    return new Promise((resolve, reject) => {
+        pool.query("update historialEstados set monto =? where id_historialEstado=?", [monto, id_historialEstado], (error, res) => {
+            if (error) {
+                reject(error.code);
+            } else {
+                resolve(res);
+            }
+        })
     })
 }
 
--- src/api/Inicio/post/routes/r.post.Inicio.js
+++ src/api/Inicio/post/routes/r.post.Inicio.js
@@ -1,141 +1,141 @@
 const User = require('../models/m.post.Inicio');
-const User2 = require('../../get/models/m.get.Inicio');
-
-function fechaLargaCorta(MyDate){
-
-	var MyDateString;
-
-	MyDate.setDate(MyDate.getDate() + 20);
-
-	MyDateString = (MyDate.getFullYear()+'-'+('0' + (MyDate.getMonth()+1)).slice(-2)+'-'+('0' + MyDate.getDate()).slice(-2))
-	
-	return MyDateString
-}
-function fechaActual(){
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-	var yyyy = today.getFullYear();
-
-	if(dd<10) {
-					dd = '0'+dd
-	} 
-
-	if(mm<10) {
-					mm = '0'+mm
-	} 
-
-	return yyyy+"-"+mm;
-	
-}
-module.exports = function(app){
-	app.post('/postcronogramamensual',async(req,res)=>{
-		User.postcronogramamensual(req.body,async(err,data)=>{							
-			if(err){ res.status(204).json(err);}
-			else{
-				var temp = req.body[0][0]
-				req.body  = {
-					id_ficha:temp
+const User3 = require('../../get/models/m.get.Inicio');
+const User2 = require('../../../ProcesosFisicos/get/models/m.get.valGeneral');
+const tools = require('../../../../tools/format')
+module.exports = function (app) {
+	app.post('/postcronogramamensual', async (req, res) => {
+		try {
+			var data = User.postcronogramamensual(req.body)
+			var periodos = await User2.getValGeneralPeriodos(req.body[0][0], 101, "TRUE")
+			//buscamos corte
+			var periodoCorte = null
+			for (let i = periodos.length - 1; i >= 0; i--) {
+				const periodo = periodos[i];
+				if (periodo.codigo == "C") {
+					periodoCorte = periodo
+					break;
 				}
-				var corte = await User2.getUltimoCorte(req.body.id_ficha)
-				var fecha_inicial = fechaLargaCorta(new Date(corte.fecha_inicial))
-				var fecha_final = fechaLargaCorta(new Date(corte.fecha_final))								
-				var avance = await User2.getAvanceGestionAnterior(req.body.id_ficha,corte.fecha_final)
-				corte.programado_monto = avance.valor_total||0
-				corte.programado_porcentaje = avance.porcentaje||0
-				corte.fisico_monto = avance.valor_total||0
-				corte.fisico_porcentaje = avance.porcentaje||0
-				corte.financiero_porcentaje = (corte.financiero_monto/avance.costo_directo*100)
-				var avance_Acumulado = 0
-				if(corte.codigo == "C"){
-					avance_Acumulado = corte.fisico_monto
-				}
-				var data = await User2.getcronogramaInicio(corte,req.body.id_ficha,corte.fecha_final)
-					
-				if(data=="vacio"){
-					data = {}
-					data.programado_monto_total
-					data.programado_porcentaje_total
-					data.fisico_monto_total
-					data.fisico_porcentaje_total
-					data.financiero_monto_total
-					data.financiero_porcentaje_total
-					data.grafico_programado=[]
-					data.grafico_fisico=[]
-					data.grafico_financiero=[]
-					data.grafico_periodos=[]
-					data.data=[]								
-				}
-				data.fecha_inicial = fecha_inicial
-				data.fecha_final = fecha_final
-				data.avance_Acumulado = avance_Acumulado
-				data.fechaActual = fechaActual()
-				res.json(data)
 			}
-		})
+			var valorizacionCorte = null
+			var corte = {}
+			var avance_Acumulado = 0
+			if (periodoCorte) {
+				valorizacionCorte = await User2.getValGeneralResumenPeriodo(req.body[0][0], periodoCorte.fecha_inicial, periodoCorte.fecha_final, false)
+				var financiero_monto = await User3.getFinancieroMonto(req.body[0][0])
+				corte.id_historialEstado = financiero_monto.id_historialEstado
+				corte.codigo = "C"
+				corte.fecha = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+				corte.mes = periodoCorte.mes + "."
+				corte.anyo = periodoCorte.anyo
+				corte.programado_monto = valorizacionCorte.valor_total
+				corte.programado_porcentaje = valorizacionCorte.porcentaje_total
+				corte.fisico_monto = valorizacionCorte.valor_total
+				corte.fisico_porcentaje = valorizacionCorte.porcentaje_total
+				corte.financiero_monto = financiero_monto.financiero_monto
+				corte.financiero_porcentaje = financiero_monto.financiero_porcentaje
+				avance_Acumulado = valorizacionCorte.valor_total
+			} else {
+				corte = "vacio"
+				periodoCorte = {}
+				periodoCorte.fecha_final = periodos[0].fecha_inicial
+			}
+			var cronograma = await User3.getcronogramaInicio(corte, req.body[0][0], periodoCorte.fecha_final)
+			if (cronograma == "vacio") {
+				cronograma = {}
+				cronograma.programado_monto_total = 0
+				cronograma.programado_porcentaje_total = 0
+				cronograma.fisico_monto_total = 0
+				cronograma.fisico_porcentaje_total = 0
+				cronograma.financiero_monto_total = 0
+				cronograma.financiero_porcentaje_total = 0
+				cronograma.grafico_programado = []
+				cronograma.grafico_fisico = []
+				cronograma.grafico_financiero = []
+				cronograma.grafico_periodos = []
+				cronograma.data = []
+				fecha_final = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+			} else {
+				fecha_final = cronograma.data[cronograma.data.length - 1].fecha
+			}
+			cronograma.fecha_inicial = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+			cronograma.fecha_final = fecha_final
+			cronograma.avance_Acumulado = avance_Acumulado
+			cronograma.fechaActual = tools.fechaActual()
+			res.json(
+				cronograma
+			)
+		} catch (error) {
+			console.log(error)
+			res.status(400).json(error)
+		}
 	})
-	app.put('/postFinancieroCorte',(req,res)=>{
-		console.log("body",req.body);
-			
-		User.postFinancieroCorte(req.body.monto,req.body.id_historialEstado,(err,data)=>{							
-			if(err){ res.status(204).json(err);}
-			else{
-				if(req.body.id_ficha == null){
-					res.json("null");		
-				}else{					
-					User2.getUltimoCorte(req.body.id_ficha,(err,corte)=>{
-						if(err){
-							res.json(err);	
-						}else{
-							console.log(req.body.id_ficha,corte.fecha_final);
-							var fecha_inicial = fechaLargaCorta(new Date(corte.fecha_inicial))
-							var fecha_final = fechaLargaCorta(new Date(corte.fecha_final))								
-							User2.getAvanceGestionAnterior(req.body.id_ficha,corte.fecha_final,(err,avance)=>{
-								if(err){
-									res.json(err);
-								}else{
-									corte.programado_monto = avance.valor_total||0
-									corte.programado_porcentaje = avance.porcentaje||0
-									corte.fisico_monto = avance.valor_total||0
-									corte.fisico_porcentaje = avance.porcentaje||0
-									corte.financiero_porcentaje = (corte.financiero_monto/avance.costo_directo*100)
-									var avance_Acumulado = 0
-									if(corte.codigo == "C"){
-										avance_Acumulado = corte.fisico_monto
-									}
-									User2.getcronogramaInicio(corte,req.body.id_ficha,corte.fecha_final,(err,data)=>{
-										if(err){
-											res.json(err);
-										}else{
-											if(data=="vacio"){
-												data = {}
-												data.programado_monto_total
-												data.programado_porcentaje_total
-												data.fisico_monto_total
-												data.fisico_porcentaje_total
-												data.financiero_monto_total
-												data.financiero_porcentaje_total
-												data.grafico_programado=[]
-												data.grafico_fisico=[]
-												data.grafico_financiero=[]
-												data.grafico_periodos=[]
-												data.data=[]								
-											}
-											data.fecha_inicial = fecha_inicial
-											data.fecha_final = fecha_final
-											data.avance_Acumulado = avance_Acumulado
-											data.fechaActual = fechaActual()
-											res.json(data)
-										}
-									})
-								}	
-							
-							})
-						}	
-					
-					})
+	app.put('/postFinancieroCorte', async (req, res) => {
+		try {
+			var data = await User.postFinancieroCorte(req.body.monto, req.body.id_historialEstado)
+			if(req.body.id_ficha == null){
+				throw "null"
+			}
+			var periodos = await User2.getValGeneralPeriodos(req.body.id_ficha, 101, "TRUE")
+			//buscamos corte
+			var periodoCorte = null
+			for (let i = periodos.length - 1; i >= 0; i--) {
+				const periodo = periodos[i];
+				if (periodo.codigo == "C") {
+					periodoCorte = periodo
+					break;
 				}
 			}
-		})
+			var valorizacionCorte = null
+			var corte = {}
+			var avance_Acumulado = 0
+			if (periodoCorte) {
+				valorizacionCorte = await User2.getValGeneralResumenPeriodo(req.body.id_ficha, periodoCorte.fecha_inicial, periodoCorte.fecha_final, false)
+				var financiero_monto = await User3.getFinancieroMonto(req.body.id_ficha)
+				corte.id_historialEstado = financiero_monto.id_historialEstado
+				corte.codigo = "C"
+				corte.fecha = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+				corte.mes = periodoCorte.mes + "."
+				corte.anyo = periodoCorte.anyo
+				corte.programado_monto = valorizacionCorte.valor_total
+				corte.programado_porcentaje = valorizacionCorte.porcentaje_total
+				corte.fisico_monto = valorizacionCorte.valor_total
+				corte.fisico_porcentaje = valorizacionCorte.porcentaje_total
+				corte.financiero_monto = financiero_monto.financiero_monto
+				corte.financiero_porcentaje = financiero_monto.financiero_porcentaje
+				avance_Acumulado = valorizacionCorte.valor_total
+			} else {
+				corte = "vacio"
+				periodoCorte = {}
+				periodoCorte.fecha_final = periodos[0].fecha_inicial
+			}
+			var cronograma = await User3.getcronogramaInicio(corte, req.body.id_ficha, periodoCorte.fecha_final)
+			if (cronograma == "vacio") {
+				cronograma = {}
+				cronograma.programado_monto_total = 0
+				cronograma.programado_porcentaje_total = 0
+				cronograma.fisico_monto_total = 0
+				cronograma.fisico_porcentaje_total = 0
+				cronograma.financiero_monto_total = 0
+				cronograma.financiero_porcentaje_total = 0
+				cronograma.grafico_programado = []
+				cronograma.grafico_fisico = []
+				cronograma.grafico_financiero = []
+				cronograma.grafico_periodos = []
+				cronograma.data = []
+				fecha_final = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+			} else {
+				fecha_final = cronograma.data[cronograma.data.length - 1].fecha
+			}
+			cronograma.fecha_inicial = tools.fechaLargaCorta(new Date(periodoCorte.fecha_final))
+			cronograma.fecha_final = fecha_final
+			cronograma.avance_Acumulado = avance_Acumulado
+			cronograma.fechaActual = tools.fechaActual()
+			res.json(
+				cronograma
+			)
+		} catch (error) {
+			console.log(error)
+			res.status(400).json(error)
+		}
 	})
 }
