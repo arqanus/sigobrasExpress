7b6106182d782b7bad6eeb8dc6562e58423fdd8e
kailucho <luijhy234@gmail.com>
2019-06-06 09:32:19 -0500

    db conf

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 var pool = mysql.createPool({
     //master
-    host: 'localhost',
-    user: 'root',
-    password: 'password',    
-    database: 'sigobras_db',
-    // //dev
-    // host: '192.168.0.4',
-    // user: 'kaido',
-    // password: 'kaido',    
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'password',    
     // database: 'sigobras_db',
+    // //dev
+    host: '192.168.0.4',
+    user: 'kaido',
+    password: 'kaido',    
+    database: 'sigobras_db',
     // //master_test
     // host: 'localhost',
     // user: 'kaido',
