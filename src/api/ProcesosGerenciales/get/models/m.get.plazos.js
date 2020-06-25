48983ec2fa9a987fc5df4a48f9a6ed94bf230903
kailucho <luijhy9234@gmail.com>
2020-06-25 14:34:13 -0500

    interfaz de plazos

--- src/api/ProcesosGerenciales/get/models/m.get.plazos.js
+++ src/api/ProcesosGerenciales/get/models/m.get.plazos.js
@@ -1,9 +1,9 @@
 const pool = require('../../../../db/connection');
 module.exports = {
-    getPlazos(id_ficha) {
+    getPlazos(id_ficha,nivel,idplazos="null") {
         return new Promise((resolve, reject) => {
-            var query = "select * from plazos_historial where fichas_id_ficha = ?"
-            pool.query(query, [id_ficha], (err, res) => {
+            var query = "SELECT plazos_historial.*, DATE_FORMAT(fecha_inicio, '%Y-%m-%d') fecha_inicio, DATE_FORMAT(fecha_final, '%Y-%m-%d') fecha_final FROM plazos_historial WHERE fichas_id_ficha = ? AND nivel = ? AND COALESCE(plazos_historial.idplazos, 'null') = '"+idplazos+"' ORDER BY fecha_inicio"
+            pool.query(query, [id_ficha,nivel], (err, res) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -14,7 +14,7 @@ module.exports = {
     },
     putPlazos(data) {
         return new Promise((resolve, reject) => {
-            var query = "INSERT INTO plazos_historial (idplazos_historial, tipo, descripcion, fecha_inicio, fecha_final, documento_resolucion_estado, imagen, observacion, fichas_id_ficha,idplazos) VALUES ? ON DUPLICATE KEY UPDATE tipo = values(tipo), descripcion = values(descripcion), fecha_inicio = values(fecha_inicio), fecha_final = values(fecha_final), documento_resolucion_estado = values(documento_resolucion_estado), imagen = values(imagen), observacion = values(observacion) "
+            var query = "INSERT INTO plazos_historial (idplazos_historial, tipo, nivel,descripcion, fecha_inicio, fecha_final, documento_resolucion_estado, imagen, observacion, fichas_id_ficha,idplazos) VALUES ? ON DUPLICATE KEY UPDATE tipo = values(tipo), descripcion = values(descripcion), fecha_inicio = values(fecha_inicio), fecha_final = values(fecha_final), documento_resolucion_estado = values(documento_resolucion_estado), imagen = values(imagen), observacion = values(observacion) "
             pool.query(query, [data], (err, res) => {
                 if (err) {
                     reject(err);
@@ -26,8 +26,20 @@ module.exports = {
     },
     deletePlazos(idplazos_historial) {
         return new Promise((resolve, reject) => {
-            var query = "DELETE FROM plazos_historial WHERE idplazos_historial = 1"
+            var query = "DELETE FROM plazos_historial WHERE idplazos_historial = ?"
             pool.query(query, [idplazos_historial], (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            })
+        })
+    },
+    getPlazosTipo(nivel) {
+        return new Promise((resolve, reject) => {
+            var query = "select * from plazos_tipo where nivel = ?"
+            pool.query(query, [nivel], (err, res) => {
                 if (err) {
                     reject(err);
                 } else {
--- src/api/ProcesosGerenciales/get/routes/r.get.plazos.js
+++ src/api/ProcesosGerenciales/get/routes/r.get.plazos.js
@@ -3,8 +3,17 @@ const User = require('../models/m.get.plazos');
 module.exports = (app) => {
     app.post('/getPlazos', async (req, res) => {
         try {
-            var data = await User.getPlazos(req.body.id_ficha)
-            res.json(data)
+            var plazosPadre = await User.getPlazos(req.body.id_ficha,1)
+            var plazos = []
+            for (let i = 0; i < plazosPadre.length; i++) {
+                const padre = plazosPadre[i];
+                var hijo = await User.getPlazos(req.body.id_ficha,2,padre.idplazos_historial)
+                plazos.push(padre)
+                plazos = plazos.concat(hijo)
+            }
+            // console.log("plazos",plazos);
+            
+            res.json(plazos)
         } catch (err) {
             console.log(err);
             res.status(204).json(err)
@@ -12,19 +21,26 @@ module.exports = (app) => {
     })
     app.post('/putPlazos', async (req, res) => {
         try {
+            var padre_id = 0
             for (let i = 0; i < req.body.length; i++) {
                 const padre = req.body[i];
-                const hijos = padre[padre.length-1]
-                padre.pop()
-                // console.log("padre a",padre);
-                // console.log("hijos a",hijos);
-                var padre_res = await User.putPlazos([padre])
-                for (let j = 0; j < hijos.length; j++) {
-                    const hijo = hijos[j];
-                    hijo.push(padre_res.insertId)
+                
+                if(padre[2]==1){
+                    padre_id = padre[0]
+                    // console.log("padre_id",padre_id);
+                    // console.log("padre",padre);
+                    var padre_res = await User.putPlazos([padre])
+                    if(padre_id == null){
+                        padre_id = padre_res.insertId
+                    }
+                    // console.log("padre_id",padre_id);
+                }else{
+                    padre[padre.length-1] = padre_id
+                    // console.log("hijo",padre);
+                    var padre_res = await User.putPlazos([padre])                                     
                 }
-                // console.log("hijos d",hijos);
-                var hijos_res = await User.putPlazos(hijos)
+                // console.log(padre_res);
+                
             }
             res.json("exito")
         } catch (err) {
@@ -35,6 +51,15 @@ module.exports = (app) => {
     app.post('/deletePlazos', async (req, res) => {
         try {
             var data = await User.deletePlazos(req.body.idplazos_historial)
+            res.json(data)
+        } catch (err) {
+            console.log(err);
+            res.status(204).json(err)
+        }
+    })
+    app.post('/getPlazosTipo', async (req, res) => {
+        try {
+            var data = await User.getPlazosTipo(req.body.nivel)
             res.json(data)
         } catch (err) {
             console.log(err);
