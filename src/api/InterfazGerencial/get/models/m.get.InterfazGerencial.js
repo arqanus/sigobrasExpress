6eaad1e8c0fd526845abd2b57091a558730b6a0a
kaido <luijhy9234@gmail.com>
2020-07-14 14:00:24 -0500

    modificacion de iapis de interfaz general

--- src/api/InterfazGerencial/get/models/m.get.InterfazGerencial.js
+++ src/api/InterfazGerencial/get/models/m.get.InterfazGerencial.js
@@ -1,9 +1,9 @@
 const pool = require('../../../../db/connection');
 
 module.exports = {
-  getProvincias() {
+  getProvincias(id_acceso) {
     return new Promise((resolve, reject) => {
-      pool.query("select * from unidadejecutoras where nivel =2;", [], (error, res) => {
+      pool.query("SELECT unidadejecutoras.* FROM unidadejecutoras LEFT JOIN fichas ON fichas.unidadEjecutoras_id_unidadEjecutora = unidadejecutoras.id_unidadEjecutora LEFT JOIN fichas_has_accesos ON fichas_has_accesos.Fichas_id_ficha = fichas.id_ficha WHERE nivel = 2 AND fichas_has_accesos.Accesos_id_acceso = ? GROUP BY unidadejecutoras.id_unidadEjecutora;", [id_acceso], (error, res) => {
         if (error) {
           reject(error);
         }
@@ -12,9 +12,9 @@ module.exports = {
     })
 
   },
-  getSectores() {
+  getSectores(id_acceso,id_unidadEjecutora) {
     return new Promise((resolve, reject) => {
-      pool.query("select * from sectores;", [], (error, res) => {
+      pool.query("SELECT sectores.* FROM sectores LEFT JOIN fichas ON fichas.sectores_idsectores = sectores.idsectores LEFT JOIN fichas_has_accesos ON fichas_has_accesos.Fichas_id_ficha = fichas.id_ficha WHERE fichas_has_accesos.Accesos_id_acceso = ? AND ((0 = ?) OR fichas.unidadEjecutoras_id_unidadEjecutora = ?) GROUP BY sectores.idsectores;", [id_acceso,id_unidadEjecutora,id_unidadEjecutora], (error, res) => {
         if (error) {
           reject(error);
         }
@@ -23,9 +23,9 @@ module.exports = {
     })
 
   },
-  getModalidadesEjecutoras() {
+  getModalidadesEjecutoras(id_acceso,id_unidadEjecutora) {
     return new Promise((resolve, reject) => {
-      pool.query("select * from modalidad_ejecutora;", [], (error, res) => {
+      pool.query("SELECT modalidad_ejecutora.* FROM modalidad_ejecutora LEFT JOIN fichas ON fichas.modalidad_ejecutora = modalidad_ejecutora.idmodalidad_ejecutora LEFT JOIN fichas_has_accesos ON fichas_has_accesos.Fichas_id_ficha = fichas.id_ficha WHERE fichas_has_accesos.Accesos_id_acceso = ? AND ((0 = ?) OR fichas.unidadEjecutoras_id_unidadEjecutora = ?) GROUP BY modalidad_ejecutora.idmodalidad_ejecutora;", [id_acceso,id_unidadEjecutora,id_unidadEjecutora], (error, res) => {
         if (error) {
           reject(error);
         }
@@ -34,9 +34,9 @@ module.exports = {
     })
 
   },
-  getEstados() {
+  getEstados(id_acceso,id_unidadEjecutora) {
     return new Promise((resolve, reject) => {
-      pool.query("select * from  estados;", [], (error, res) => {
+      pool.query("SELECT estado.Estados_id_Estado id_Estado, estado.nombre FROM fichas LEFT JOIN fichas_has_accesos ON fichas_has_accesos.Fichas_id_ficha = fichas.id_ficha LEFT JOIN (SELECT Fichas_id_ficha, nombre, codigo, historialestados.Estados_id_Estado FROM historialestados INNER JOIN (SELECT MAX(id_historialEstado) id_historialEstado FROM historialestados GROUP BY fichas_id_ficha) historial ON historial.id_historialEstado = historialestados.id_historialEstado LEFT JOIN estados ON estados.id_Estado = historialestados.Estados_id_Estado) estado ON estado.Fichas_id_ficha = fichas.id_ficha WHERE fichas_has_accesos.Accesos_id_acceso = ? AND ((0 = ?) OR fichas.unidadEjecutoras_id_unidadEjecutora = ?) GROUP BY Estados_id_Estado", [id_acceso,id_unidadEjecutora,id_unidadEjecutora], (error, res) => {
         if (error) {
           reject(error);
         }
@@ -48,6 +48,28 @@ module.exports = {
   getInterfazGerencialData(unidadEjecutoras_id_unidadEjecutora, sectores_idsectores, modalidad_ejecutora, Estados_id_Estado) {
     return new Promise((resolve, reject) => {
       pool.query("SELECT fichas.codigo, unidadejecutoras.nombre unidad_ejecutora_nombre, sectores.nombre sector_nombre, modalidad_ejecutora.nombre modalidad_ejecutora_nombre, estado.nombre estado_nombre, fichas.fecha_inicial FROM fichas LEFT JOIN (SELECT Fichas_id_ficha, nombre, codigo, historialestados.Estados_id_Estado FROM historialestados INNER JOIN (SELECT MAX(id_historialEstado) id_historialEstado FROM historialestados GROUP BY fichas_id_ficha) historial ON historial.id_historialEstado = historialestados.id_historialEstado LEFT JOIN estados ON estados.id_Estado = historialestados.Estados_id_Estado) estado ON estado.Fichas_id_ficha = fichas.id_ficha LEFT JOIN unidadejecutoras ON unidadejecutoras.id_unidadEjecutora = fichas.unidadEjecutoras_id_unidadEjecutora LEFT JOIN sectores ON sectores.idsectores = fichas.sectores_idsectores LEFT JOIN modalidad_ejecutora ON modalidad_ejecutora.idmodalidad_ejecutora = fichas.modalidad_ejecutora WHERE ((0 = ?) OR fichas.unidadEjecutoras_id_unidadEjecutora = ?) AND ((0 = ?) OR sectores_idsectores = ?) AND ((0 = ?) OR modalidad_ejecutora = ?) AND ((0 = ?) OR Estados_id_Estado = ?) GROUP BY fichas.unidadEjecutoras_id_unidadEjecutora , sectores_idsectores,id_ficha", [unidadEjecutoras_id_unidadEjecutora, unidadEjecutoras_id_unidadEjecutora, sectores_idsectores, sectores_idsectores, modalidad_ejecutora, modalidad_ejecutora, Estados_id_Estado, Estados_id_Estado], (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(res)
+      })
+    })
+
+  },
+  IG_AvanceFisico() {
+    return new Promise((resolve, reject) => {
+      pool.query("SELECT MONTH(avanceactividades.fecha) mes, sum(avanceactividades.valor) avance FROM avanceactividades WHERE YEAR(avanceactividades.fecha) = 2020 GROUP BY MONTH(avanceactividades.fecha);", [], (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(res)
+      })
+    })
+
+  },
+  IG_AvanceFisicoProgramado() {
+    return new Promise((resolve, reject) => {
+      pool.query("SELECT MONTH(cronogramamensual.mes) mes,sum(cronogramamensual.programado) avance FROM cronogramamensual WHERE YEAR(cronogramamensual.mes) = 2020 GROUP BY MONTH(cronogramamensual.mes);", [], (error, res) => {
         if (error) {
           reject(error);
         }
--- src/api/InterfazGerencial/get/routes/r.get.InterfazGerencial.js
+++ src/api/InterfazGerencial/get/routes/r.get.InterfazGerencial.js
@@ -1,32 +1,32 @@
 const User = require('../models/m.get.InterfazGerencial');
 module.exports = (app) => {
-	app.get('/getProvincias', async (req, res) => {
+	app.post('/getProvincias', async (req, res) => {
 		try {
-			var data = await User.getProvincias()
+			var data = await User.getProvincias(req.body.id_acceso)
 			res.json(data)
 		} catch (error) {
 			res.status(200).json(error)
 		}
 	});
-	app.get('/getSectores', async (req, res) => {
+	app.post('/getSectores', async (req, res) => {
 		try {
-			var data = await User.getSectores()
+			var data = await User.getSectores(req.body.id_acceso,req.body.id_unidadEjecutora)
 			res.json(data)
 		} catch (error) {
 			res.status(200).json(error)
 		}
 	});
-	app.get('/getModalidadesEjecutoras', async (req, res) => {
+	app.post('/getModalidadesEjecutoras', async (req, res) => {
 		try {
-			var data = await User.getModalidadesEjecutoras()
+			var data = await User.getModalidadesEjecutoras(req.body.id_acceso,req.body.id_unidadEjecutora)
 			res.json(data)
 		} catch (error) {
 			res.status(200).json(error)
 		}
 	});
-	app.get('/getEstados', async (req, res) => {
+	app.post('/getEstados', async (req, res) => {
 		try {
-			var data = await User.getEstados()
+			var data = await User.getEstados(req.body.id_acceso,req.body.id_unidadEjecutora)
 			res.json(data)
 		} catch (error) {
 			res.status(200).json(error)
@@ -35,6 +35,22 @@ module.exports = (app) => {
 	app.post('/getInterfazGerencialData', async (req, res) => {
 		try {
 			var data = await User.getInterfazGerencialData(req.body.id_unidadEjecutora, req.body.idsectores, req.body.idmodalidad_ejecutora, req.body.id_Estado)
+			res.json(data)
+		} catch (error) {
+			res.status(200).json(error)
+		}
+	});
+	app.post('/IG_AvanceFisico', async (req, res) => {
+		try {
+			var data = await User.IG_AvanceFisico()
+			res.json(data)
+		} catch (error) {
+			res.status(200).json(error)
+		}
+	});
+	app.post('/IG_AvanceFisicoProgramado', async (req, res) => {
+		try {
+			var data = await User.IG_AvanceFisicoProgramado()
 			res.json(data)
 		} catch (error) {
 			res.status(200).json(error)
