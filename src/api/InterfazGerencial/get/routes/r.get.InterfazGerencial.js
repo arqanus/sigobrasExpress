64effd29da63b91ba16678e904b5f4b63386b775
kaido <luijhy9234@gmail.com>
2020-07-17 13:57:36 -0500

    api de porcentaje de avance fiso y financiero

--- src/api/InterfazGerencial/get/routes/r.get.InterfazGerencial.js
+++ src/api/InterfazGerencial/get/routes/r.get.InterfazGerencial.js
@@ -1,4 +1,5 @@
 const User = require('../models/m.get.InterfazGerencial');
+const { IG_AvanceFisico_porcentaje, IG_AvanceFinanciero_porcentaje } = require('../models/m.get.InterfazGerencial');
 module.exports = (app) => {
 	app.post('/getProvincias', async (req, res) => {
 		try {
@@ -34,8 +35,16 @@ module.exports = (app) => {
 	});
 	app.post('/getInterfazGerencialData', async (req, res) => {
 		try {
-			var data = await User.getInterfazGerencialData(req.body.id_acceso,req.body.id_unidadEjecutora, req.body.idsectores, req.body.idmodalidad_ejecutora, req.body.id_Estado)
-			res.json(data)
+			var obras = await User.getInterfazGerencialData(req.body.id_acceso,req.body.id_unidadEjecutora, req.body.idsectores, req.body.idmodalidad_ejecutora, req.body.id_Estado)
+			for (let index = 0; index < obras.length; index++) {
+				const obra = obras[index];
+				var avanceFisico_porcentaje =  await IG_AvanceFisico_porcentaje(obra.id_ficha)
+				obra.avanceFisico_porcentaje = avanceFisico_porcentaje[0].porcentaje_avance||0
+				var avanceFinanciero_porcentaje =  await IG_AvanceFinanciero_porcentaje(obra.id_ficha)			
+				obra.avanceFinanciero_porcentaje = avanceFinanciero_porcentaje[0].porcentaje_avance||0
+			}
+			res.json(obras)
+
 		} catch (error) {
 			res.status(200).json(error)
 		}
