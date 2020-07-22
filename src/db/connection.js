eba4ee8aff7e6ad89491526b10528fdf9365e702
Nabilapaza <scorpion_nabil@hotmail.com>
2020-07-21 21:56:13 -0500

    costos indirectos con apis

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,17 +2,17 @@ const mysql = require('mysql');
 var pool = mysql.createPool({
     
     // //MODO SERVIDOR PARA SUBIR TIENE ESTAR ACTIVO
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'Mds@anton@mysqlserver1.',  
-    // database: 'sigobras_db',
+    host: 'localhost',
+    user: 'root',
+    password: 'Mds@anton@mysqlserver1.',  
+    database: 'sigobras_db',
     
    
     // MODO REMOTO PARA TRABAJARLO LOCALMENTE
-    host:'190.117.94.80',
-    user: 'kaido',
-    password: 'K@idos1.',    
-    database: 'sigobras_db',   
+    // host:'190.117.94.80',
+    // user: 'kaido',
+    // password: 'K@idos1.',    
+    // database: 'sigobras_db',   
     
 });
 pool.getConnection((err, connection) => {
