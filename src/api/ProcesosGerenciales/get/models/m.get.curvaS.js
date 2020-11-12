d02457d8fb5277cdf4eba4e40ae57de18060482e
Luijhy Guerra <luijhy@gruposistemas.com>
2020-11-12 12:41:14 -0500

    correccion de curva s y apis de listado de personal total

--- src/api/ProcesosGerenciales/get/models/m.get.curvaS.js
+++ src/api/ProcesosGerenciales/get/models/m.get.curvaS.js
@@ -90,9 +90,9 @@ module.exports = {
         })
 
     },
-    getFechaFinalCurvaS(fecha_inicial) {
+    getFechaFinalCurvaS(fecha_inicial,id_ficha) {
         return new Promise((resolve, reject) => {
-            pool.query("SELECT DATE_FORMAT(fecha_inicial, '%Y-%m-%d') fecha_final FROM curva_s WHERE curva_s.fecha_inicial > ? LIMIT 1", [fecha_inicial], (error, res) => {
+            pool.query("SELECT DATE_FORMAT(fecha_inicial, '%Y-%m-%d') fecha_final FROM curva_s WHERE curva_s.fecha_inicial > ? AND curva_s.fichas_id_ficha = ? LIMIT 1", [fecha_inicial,id_ficha], (error, res) => {
                 if (error) {
                     reject(error);
                 }
