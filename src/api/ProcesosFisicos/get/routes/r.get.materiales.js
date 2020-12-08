ad4b7ac7b57eebe52c60d50d76ffcbeeb5692912
Luijhy Guerra <luijhy9234@gmail.com>
2020-12-08 01:08:06 -0500

    api para ingreso de documento de adquisicion

--- src/api/ProcesosFisicos/get/routes/r.get.materiales.js
+++ src/api/ProcesosFisicos/get/routes/r.get.materiales.js
@@ -289,4 +289,22 @@ module.exports = function (app) {
 			res.status(400).json(error.code);
 		}
 	})
+	app.post('/getDocumentoAdquisicionDetalles', async (req, res) => {
+		try {
+			var data = await User.getDocumentoAdquisicionDetalles(req.body)
+			res.json(data);
+		} catch (error) {
+			console.log(error);
+			res.status(400).json(error.code);
+		}
+	})
+	app.post('/postDocumentoAdquisicionDetalles', async (req, res) => {
+		try {
+			var data = await User.postDocumentoAdquisicionDetalles(req.body)
+			res.json(data);
+		} catch (error) {
+			console.log(error);
+			res.status(400).json(error.code);
+		}
+	})
 }
