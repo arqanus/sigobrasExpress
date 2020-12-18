53b290a72aea9fff04609d1ddd952dbdbd5af353
Luijhy Guerra <luijhy9234@gmail.com>
2020-12-18 16:19:11 -0500

    api de labels

--- src/api/Inicio/m.fichasLabels.js
+++ src/api/Inicio/m.fichasLabels.js
@@ -1,10 +1,31 @@
 const pool = require('../../db/connection');
 
 module.exports = {
-    getFichasLabels() {
+    getFichasLabels({ id_ficha = 0, texto_buscar }) {
         return new Promise((resolve, reject) => {
+            var query = `
+            SELECT 
+                SUM(IF(fichas_has_labels.fichas_id_ficha = ${id_ficha},
+                    1,
+                    0)) orden,
+                fichas_labels.*
+            FROM
+                fichas_labels
+                    LEFT JOIN
+                fichas_has_labels ON fichas_has_labels.fichas_labels_id = fichas_labels.id
+            
+            `
+            var condiciones = []
+            if (texto_buscar != "" && texto_buscar != undefined) {
+                condiciones.push(`(fichas_labels.nombre like \'%${texto_buscar}%\' || fichas_labels.descripcion like \'%${texto_buscar}%\')`)
+            }
+            if (condiciones.length > 0) {
+                query += " WHERE " + condiciones.join(" AND ")
+            }
+            query += ` GROUP BY fichas_has_labels.fichas_labels_id
+            ORDER BY 1 DESC , fichas_labels.id DESC`
             pool.query(
-                `select * from fichas_labels`
+                query
                 , (error, res) => {
                     if (error) {
                         reject(error);
