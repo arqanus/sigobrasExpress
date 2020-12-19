3c21180a7ee0bfa5ddea618618cfc8177b9fd6a6
Nabilapaza <scorpion_nabil@hotmail.com>
2020-12-19 10:01:55 -0600

    Creacion de APis para interfaz de CostosIndirectosFichaTecnica

---
+++ src/api/CostosIndirectos/r.CostosIndirectos.js
@@ -0,0 +1,49 @@
+const User = require('./m.CostosIndirectos');
+
+module.exports = function (app) {
+    app.get('/costosIndirectos', async (req, res) => {
+        try {
+            var data = await User.getCostosIndirectos(req.query)
+            console.log(req.query);
+            res.json(data)
+        } catch (err) {
+            console.log(err);
+            res.status(404).json({ error: err.code })
+        }
+    })
+    app.post('/costosIndirectos', async (req, res) => {
+        try {
+            var data = await User.postCostosIndirectos(req.body)
+            res.json(data)
+        } catch (err) {
+            console.log(err);
+            res.status(404).json({ error: err.code })
+        }
+    })
+    app.delete('/costoIndirecto',async(req,res)=>{
+		try {
+			var data = await User.deleteCostoIndirecto(req.body)
+			res.json(data);
+		} catch (error) {
+			res.status(400).json(error)
+		}
+    })
+    app.put('/costoIndirecto', async (req, res) => {
+        try {
+            var message = ""
+            var response = await User.putCostoIndirecto(req.body)
+            if (response.affectedRows>0) {
+                message = "Actualizado con exito"
+                res.status(200).json({message})
+                return 
+            }else{
+                message = "Hubo un problema"
+                res.status(400).json({message})
+                return
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json('error');
+        }
+    })
+}
