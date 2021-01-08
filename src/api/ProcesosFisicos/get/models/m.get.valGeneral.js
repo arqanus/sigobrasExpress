e3239b3cba6066a91f72c0832e900d7c818bfb49
Luijhy Guerra <luijhy9234@gmail.com>
2021-01-08 11:28:24 -0500

    fix api valgeneral anyos

--- src/api/ProcesosFisicos/get/models/m.get.valGeneral.js
+++ src/api/ProcesosFisicos/get/models/m.get.valGeneral.js
@@ -6,7 +6,25 @@ const tools = require('../../../../tools/format')
 module.exports = {
   getValGeneralAnyos(id_ficha) {
     return new Promise((resolve, reject) => {
-      pool.query("SELECT year(avanceactividades.fecha) anyo FROM fichas LEFT JOIN componentes ON componentes.fichas_id_ficha = fichas.id_ficha LEFT JOIN partidas ON partidas.componentes_id_componente = componentes.id_componente LEFT JOIN actividades ON actividades.Partidas_id_partida = partidas.id_partida INNER JOIN avanceactividades ON avanceactividades.Actividades_id_actividad = actividades.id_actividad LEFT JOIN historialestados ON historialestados.Fichas_id_ficha = fichas.id_ficha AND historialestados.fecha_inicial <= avanceactividades.fecha AND avanceactividades.fecha < historialestados.fecha_final LEFT JOIN estados ON estados.id_Estado = historialestados.Estados_id_Estado left join historialactividades on historialactividades.actividades_id_actividad = actividades.id_actividad WHERE historialactividades.estado is null and fichas.id_ficha = ? GROUP BY year(avanceactividades.fecha)", id_ficha, (err, res) => {
+      var query = `
+      SELECT 
+          YEAR(avanceactividades.fecha) anyo
+      FROM
+          fichas
+              LEFT JOIN
+          componentes ON componentes.fichas_id_ficha = fichas.id_ficha
+              LEFT JOIN
+          partidas ON partidas.componentes_id_componente = componentes.id_componente
+              LEFT JOIN
+          actividades ON actividades.Partidas_id_partida = partidas.id_partida
+              INNER JOIN
+          avanceactividades ON avanceactividades.Actividades_id_actividad = actividades.id_actividad
+      WHERE
+          fichas.id_ficha = ${id_ficha}
+              AND COALESCE(avanceactividades.valor, 0) != 0
+      GROUP BY YEAR(avanceactividades.fecha)
+      `
+      pool.query(query, (err, res) => {
         if (err) {
           return reject(err)
         }
