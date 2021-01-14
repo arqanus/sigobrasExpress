7f9de65c46eefeba6dc2c2655475270483b84c28
Luijhy Guerra <luijhy9234@gmail.com>
2021-01-14 11:12:26 -0500

    api plazos archivo

--- src/api/ProcesosGerenciales/PlazosHistorial/m.plazos.js
+++ src/api/ProcesosGerenciales/PlazosHistorial/m.plazos.js
@@ -1,18 +1,19 @@
-const pool = require('../../../db/connection');
+const pool = require("../../../db/connection");
 module.exports = {
-    postPlazos(data) {
-        return new Promise((resolve, reject) => {
-            pool.query("INSERT INTO plazos_historial SET ?", [data], (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res)
-            })
-        })
-    },
-    putPlazos(data) {
-        return new Promise((resolve, reject) => {
-            pool.query(`
+  postPlazos(data) {
+    return new Promise((resolve, reject) => {
+      pool.query("INSERT INTO plazos_historial SET ?", [data], (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      });
+    });
+  },
+  putPlazos(data) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
             INSERT INTO plazos_historial 
             (
                 id, 
@@ -41,40 +42,55 @@ module.exports = {
             n_dias = values(n_dias),
             plazo_aprobado = values(plazo_aprobado),
             fecha_aprobada = values(fecha_aprobada)
-            `, [data], (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res)
-            })
-        })
-    },
-    getPlazosPadres({ id_ficha }) {
-        return new Promise((resolve, reject) => {
-            pool.query(`
-            SELECT 
-                plazos_historial.*, plazos_tipo.nombre tipo_nombre,
-                DATE_FORMAT(fecha_inicio,"%Y-%m-%d")fecha_inicio,
-                DATE_FORMAT(fecha_final,"%Y-%m-%d")fecha_final,
-                DATE_FORMAT(fecha_aprobada,"%Y-%m-%d")fecha_aprobada
-            FROM
-                plazos_historial
-                    LEFT JOIN
-                plazos_tipo ON plazos_tipo.idplazos_tipo = plazos_historial.tipo
-            WHERE
-                plazos_historial.nivel = 1
-                    AND fichas_id_ficha = ${id_ficha}
-            `, (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res)
-            })
-        })
-    },
-    getPlazosHijos({ id_ficha, id_padre }) {
-        return new Promise((resolve, reject) => {
-            pool.query(`
+            `,
+        [data],
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        }
+      );
+    });
+  },
+  getPlazosPadres({ id_ficha }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
+        SELECT 
+            plazos_historial.*,
+            plazos_tipo.nombre tipo_nombre,
+            DATE_FORMAT(fecha_inicio, '%Y-%m-%d') fecha_inicio,
+            DATE_FORMAT(fecha_final, '%Y-%m-%d') fecha_final,
+            DATE_FORMAT(fecha_aprobada, '%Y-%m-%d') fecha_aprobada,
+            CONCAT(usuarios.nombre,
+                    ' ',
+                    usuarios.apellido_paterno) usuario_nombre
+        FROM
+            plazos_historial
+                LEFT JOIN
+            plazos_tipo ON plazos_tipo.idplazos_tipo = plazos_historial.tipo
+                LEFT JOIN
+            accesos ON accesos.id_acceso = plazos_historial.archivo_editor
+                LEFT JOIN
+            usuarios ON usuarios.id_usuario = accesos.Usuarios_id_usuario
+        WHERE
+          plazos_historial.nivel = 1
+                AND fichas_id_ficha = ${id_ficha}
+            `,
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        }
+      );
+    });
+  },
+  getPlazosHijos({ id_ficha, id_padre }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
             SELECT 
                 plazos_historial.*, plazos_tipo.nombre tipo_nombre,
                 DATE_FORMAT(fecha_inicio,"%Y-%m-%d")fecha_inicio,
@@ -88,30 +104,31 @@ module.exports = {
                 plazos_historial.nivel = 2
                     AND fichas_id_ficha = ${id_ficha}
                     AND id_padre = ${id_padre}
-            `, (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res)
-            })
-        })
-    },
-    deletePlazosPadresAndHijos({ id }) {
-        return new Promise((resolve, reject) => {
-            pool.query(`
+            `,
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        }
+      );
+    });
+  },
+  deletePlazosPadresAndHijos({ id }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
             DELETE FROM plazos_historial 
             WHERE 
             id = ${id}
-            `, (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(res)
-            })
-        })
-    },
-
+            `,
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        }
+      );
+    });
+  },
 };
-
-
-
