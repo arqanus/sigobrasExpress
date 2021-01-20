33de2c1fb149cc62ae304d2fad5c3d8b2becfb9b
Luijhy Guerra <luijhy9234@gmail.com>
2021-01-20 18:11:37 -0500

    fix put plazos

--- api/v0/ProcesosGerenciales/PlazosHistorial/r.plazos.js
+++ api/v0/ProcesosGerenciales/PlazosHistorial/r.plazos.js
@@ -1,74 +1,71 @@
-const User = require('./m.plazos');
+const User = require("./m.plazos");
 
 module.exports = (app) => {
-    app.post('/plazos', async (req, res) => {
-        try {
-            var response = await User.postPlazos(req.body)
-            if (response.affectedRows > 0) {
-                res.status(200).json({ message: "registro exitoso" })
-            }
-            else {
-                res.status(204).json({ message: "hubo un problema" })
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(404).json({ error: err.code })
-        }
-    })
-    app.put('/plazos', async (req, res) => {
-        try {
-            var dataProcesada = [
-                req.body.id,
-                req.body.tipo,
-                req.body.nivel,
-                req.body.descripcion,
-                req.body.fecha_inicio,
-                req.body.fecha_final,
-                req.body.documento_resolucion_estado,
-                req.body.imagen,
-                req.body.observacion,
-                req.body.fichas_id_ficha,
-                req.body.n_dias,
-                req.body.plazo_aprobado,
-                req.body.fecha_aprobada
-            ]
-            console.log(dataProcesada);
-            var response = await User.putPlazos([dataProcesada])
-            if (response.affectedRows > 0) {
-                res.status(200).json({ message: "registro exitoso" })
-            }
-            else {
-                res.status(204).json({ message: "hubo un problema" })
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(404).json({ error: err.code })
-        }
-    })
-    app.get('/plazosPadres', async (req, res) => {
-        try {
-            var data = await User.getPlazosPadres(req.query)
-            res.json(data)
-        } catch (err) {
-            console.log(err);
-            res.status(404).json({ error: err.code })
-        }
-    })
-    app.get('/plazosHijos', async (req, res) => {
-        try {
-            var data = await User.getPlazosHijos(req.query)
-            res.json(data)
-        } catch (err) {
-            console.log(err);
-            res.status(404).json({ error: err.code })
-        }
-    })
-    app.delete('/plazosPadresAndHijos',async(req,res)=>{
-		try {
-			var data = await User.deletePlazosPadresAndHijos(req.body)
-			res.json(data);
-		} catch (error) {
-			res.status(400).json(error)
-		}
-	})
-}
+  app.post("/plazos", async (req, res) => {
+    try {
+      var response = await User.postPlazos(req.body);
+      if (response.affectedRows > 0) {
+        res.status(200).json({ message: "registro exitoso" });
+      } else {
+        res.status(204).json({ message: "hubo un problema" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: err.code });
+    }
+  });
+  app.put("/plazos", async (req, res) => {
+    try {
+      var dataProcesada = [
+        req.body.id,
+        req.body.tipo,
+        req.body.nivel,
+        req.body.descripcion,
+        req.body.fecha_inicio,
+        req.body.fecha_final,
+        req.body.documento_resolucion_estado,
+        req.body.observacion,
+        req.body.fichas_id_ficha,
+        req.body.n_dias,
+        req.body.plazo_aprobado,
+        req.body.fecha_aprobada,
+      ];
+      console.log(dataProcesada);
+      var response = await User.putPlazos([dataProcesada]);
+      if (response.affectedRows > 0) {
+        res.status(200).json({ message: "registro exitoso" });
+      } else {
+        res.status(204).json({ message: "hubo un problema" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: err.code });
+    }
+  });
+  app.get("/plazosPadres", async (req, res) => {
+    try {
+      var data = await User.getPlazosPadres(req.query);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: err.code });
+    }
+  });
+  app.get("/plazosHijos", async (req, res) => {
+    try {
+      var data = await User.getPlazosHijos(req.query);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: err.code });
+    }
+  });
+  app.delete("/plazosPadresAndHijos", async (req, res) => {
+    try {
+      var data = await User.deletePlazosPadresAndHijos(req.body);
+      res.json(data);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
+};
